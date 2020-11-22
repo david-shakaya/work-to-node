698d95e90e5eca32349a8badcb966bc1b4867c1d
david-shakaya <devidoz666@gmail.com>
2020-11-22 20:59:21 +0200

    ++

---
+++ index.js
@@ -0,0 +1,32 @@
+const node = require('./exporImpor') //импортируем из другого файла
+// console.log(node);
+
+//  Работа с  NODE.js
+// 1) открываем консоль(командная строка) и у нас появляетсься адрес:
+//    C: \Users\Давид >
+
+// 2)воодим адрес с названиемм проэкта 
+//    C: \Users\Давид > cd Documents\GitHub\work - to - node
+
+//3) Для того что бы получить доступ к файлу index.js  нужно сделать следующее
+//    C:\Users\Давид\Documents\GitHub\work-to-node>node index.js
+// и в консоле увидим: hello World
+
+
+/* 
+* npmjs.com  - сайт для поиска скачивание и ознакомления с пакетами
+*/
+
+
+// 4) Cоздаем packege.json - Там будем хранить все наши пакеты и другую инфу.
+//  - Находим адрес с папкой проекта куда будем добавляеть packege.json 
+    //   C:\Users\Давид\Documents\GitHub\work-to-node>npm init
+// 5)  Затем устанавливаем необходимый пакет (например валидатор) 
+//      C:\Users\Давид\Documents\GitHub\work-to-node>npm install validator
+//  6)  После установки пакет появится папка node_modules. ОБЯЗАТЕЛЬНО добавляем её в .gitignore
+// думаю можно поставить такие значения(подсмотрел у репеты) 
+  /*   node_modules /
+     .cache
+     dist/
+     .DS_Store
+   */
